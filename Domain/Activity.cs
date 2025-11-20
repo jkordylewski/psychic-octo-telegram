f73d0a9e877cafb57b9313701fdbf6b47536b6dd
Jan Kordylewski <jkordylewski@gmail.com>
2025-11-19 21:21:47 -0600

    makingthinsg required and fix for lat long

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -6,21 +6,21 @@ public class Activity
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
 
-    public string Title { get; set; }
+    public required string Title { get; set; }
 
     public DateTime Date { get; set; }
 
-    public string Description { get; set; }
+    public required string Description { get; set; }
 
-    public string Category { get; set; }    
+    public required string Category { get; set; }    
 
     public bool IsCancelled { get; set; }
 
     //location props
-    public string City { get; set; }
-    public string Venue { get; set; }
+    public required string City { get; set; }
+    public required string Venue { get; set; }
 
-    public string Latitude { get; set; }
+    public double Latitude { get; set; }
 
-    public string Longitude { get; set; }
+    public double Longitude { get; set; }
 }
