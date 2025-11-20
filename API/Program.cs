b78ec8ba5e76b14bcee0c055d1a2416e7ae69a96
Jan Kordylewski <jkordylewski@gmail.com>
2025-11-20 16:45:38 -0600

    wip and fixing version number

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public required DbSet<Activity> Activities  { get; set; }
+}
+ 
