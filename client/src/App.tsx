cece88fccf34e304b9a2353d84a15f7a9e9383ff
Jan Kordylewski <jkordylewski@gmail.com>
2025-11-30 15:22:18 -0600

    chor edependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
